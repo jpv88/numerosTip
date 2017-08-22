afa488df8b3d938ec3394dab14814a5cf847484e
Jared Perez Vega <jrd.perez88@gmail.com>
2017-08-22 18:17:17 +0200

    Setting up Network file and UI for testing

---
+++ numerosTip/numerosTip/Network.swift
@@ -0,0 +1,19 @@
+//
+//  Network.swift
+//  numerosTip
+//
+//  Created by Jared Perez Vega on 22/8/17.
+//  Copyright © 2017 Jared Perez Vega. All rights reserved.
+//
+
+import UIKit
+
+class Network: NSObject {
+
+    let url = ""
+    
+    func request() {
+        
+    }
+    
+}
