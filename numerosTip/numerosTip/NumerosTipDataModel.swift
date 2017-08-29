df244ee9c6275b4cf66367225372e5bac7422749
Jared Perez Vega <jrd.perez88@gmail.com>
2017-08-29 19:29:43 +0200

    Creacion modelo datos

---
+++ numerosTip/numerosTip/NumerosTipDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  NumerosTipDataModel.swift
+//  numerosTip
+//
+//  Created by Jared Perez Vega on 29/8/17.
+//  Copyright © 2017 Jared Perez Vega. All rights reserved.
+//
+
+import UIKit
+
+class NumerosTipDataModel: NSObject {
+
+}
