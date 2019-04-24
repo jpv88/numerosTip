6afdda9bd4728f7eeefcb8fd05784d8d1027888c
Jared Perez Vega <jrd.perez88@gmail.com>
2019-04-24 17:33:11 +0100

    - Added new class for SplitView

---
+++ numerosTip/numerosTip/SplitViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SplitViewController.swift
+//  numerosTip
+//
+//  Created by Jared Perez Vega on 24/04/2019.
+//  Copyright © 2019 Jared Perez Vega. All rights reserved.
+//
+
+import UIKit
+
+class SplitViewController: UISplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.delegate = self
+    }
+
+}
+
+extension SplitViewController: UISplitViewControllerDelegate {
+    
+    func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController: UIViewController, onto primaryViewController: UIViewController) -> Bool {
+        return true
+    }
+    
+}
