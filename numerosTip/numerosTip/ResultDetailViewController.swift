64e15060a295f59e6026206ecd34450b633af323
Jared Perez Vega <jrd.perez88@gmail.com>
2019-04-26 12:37:36 +0100

    - Added 2 classes for details

---
+++ numerosTip/numerosTip/ResultDetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ResultDetailViewController.swift
+//  numerosTip
+//
+//  Created by Jared Perez Vega on 26/04/2019.
+//  Copyright © 2019 Jared Perez Vega. All rights reserved.
+//
+
+import UIKit
+
+class ResultDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let splitView = self.navigationController?.splitViewController, !splitView.isCollapsed {
+            self.navigationItem.leftBarButtonItem = splitView.displayModeButtonItem
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
