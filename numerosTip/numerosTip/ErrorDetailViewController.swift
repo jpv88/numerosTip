61f74a69ce89510bc57f5f3d1fed8aef7bcf7430
Jared Perez Vega <jrd.perez88@gmail.com>
2019-05-15 17:58:17 +0100

    - Added ErrorDetail screen

---
+++ numerosTip/numerosTip/ErrorDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ErrorDetailViewController.swift
+//  numerosTip
+//
+//  Created by Jared Perez Vega on 15/05/2019.
+//  Copyright © 2019 Jared Perez Vega. All rights reserved.
+//
+
+import UIKit
+
+class ErrorDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+    
+}
