8b1c6f623fbf8233b866a363f95e9d3d34ab6d9f
Jared Perez Vega <jrd.perez88@gmail.com>
2019-05-16 13:22:24 +0100

    - Added keyboards files

---
+++ numerosTip/numerosTip/Components/RomanKeyboard.swift
@@ -0,0 +1,15 @@
+//
+//  RomanKeyboard.swift
+//  numerosTip
+//
+//  Created by Jared Perez Vega on 16/05/2019.
+//  Copyright © 2019 Jared Perez Vega. All rights reserved.
+//
+
+import UIKit
+
+class RomanKeyboard: UIView, UIInputViewAudioFeedback {
+    private func enableInputClicksWhenVisible() -> Bool {
+        return true
+    }
+}
