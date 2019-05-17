f3d4025156868f96d921c32876b172dd1391f4a0
Jared Perez Vega <jrd.perez88@gmail.com>
2019-05-17 12:35:48 +0100

    - UI roman keyboard

--- numerosTip/numerosTip/Components/RomanKeyboard.swift
+++ numerosTip/numerosTip/Components/RomanKeyboard.swift
@@ -9,6 +9,43 @@
 import UIKit
 
 class RomanKeyboard: UIView, UIInputViewAudioFeedback {
+    
+    var target: UIKeyInput?
+    
+    var delegate: CustomKeyboardProtocol?
+    
+    @IBOutlet private var keyboardButtons: [UIButton]!
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        keyboardButtons.forEach { button in
+            button.backgroundColor = UIColor(rgb: 0x0C77B8)
+            button.layer.cornerRadius = 5
+            button.layer.borderWidth = 1
+            button.setTitleColor(UIColor.white, for: .normal)
+            button.layer.borderColor = UIColor.black.cgColor
+        }
+    }
+    
+    @IBAction func deleteKey(_ sender: UIButton) {
+        self.target?.deleteBackward()
+    }
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        UIDevice.current.playInputClick()
+        guard let text = sender.titleLabel?.text else {return}
+        self.target?.insertText(text)
+    }
+    
+    @IBAction func changeToInteger(_ sender: UIButton) {
+        delegate?.changeToIntegerKeyboard()
+    }
+    
+    @IBAction func search(_ sender: UIButton) {
+        delegate?.search()
+    }
+    
     private func enableInputClicksWhenVisible() -> Bool {
         return true
     }
