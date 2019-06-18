355b6a42f48a694c07f7a9f67e37946812c21f2b
Jared Perez Vega <jrd.perez88@gmail.com>
2019-06-18 19:00:27 +0100

    - Fixed showing generic errors, deleted button

--- numerosTip/numerosTip/Utils/ErrorHandler.swift
+++ numerosTip/numerosTip/Utils/ErrorHandler.swift
@@ -9,25 +9,26 @@
 import UIKit
 
 final class ErrorHandler {
-    static func showError(error: Error, firstButtonTitle: String = "Ok", dismissButtonTitle: String = "Aceptar", action: (()->())? = nil, dismissAction: (()->())? = nil) {
+    private enum Localized {
+        static let accept = "general_accept".localized()
+        static let cancel = "general_cancel".localized()
+    }
+    
+    static func showError(error: Error, dismissButtonTitle: String = Localized.accept, action: (()->())? = nil, dismissAction: (()->())? = nil) {
         let alertView = UIAlertController()
-        let firstButton = UIAlertAction(title: firstButtonTitle, style: .default) { _ in
-            action?()
-        }
         
         let dismissButton = UIAlertAction(title: dismissButtonTitle, style: .cancel) { _ in
             dismissAction?()
         }
         alertView.title = "Error"
         alertView.message = error.localizedDescription
-        alertView.addAction(firstButton)
         alertView.addAction(dismissButton)
         
         guard let currentVC = UIApplication.topViewController() else { return }
         currentVC.present(alertView, animated: true)
     }
     
-    static func showAlert(title: String, msg: String, dismissButtonTitle: String = "Aceptar", action: (()->())? = nil, dismissAction: (()->())? = nil) {
+    static func showAlert(title: String, msg: String, dismissButtonTitle: String = Localized.accept, action: (()->())? = nil, dismissAction: (()->())? = nil) {
         let alertView = UIAlertController(title: title, message: msg, preferredStyle: .alert)
         
         let dismissButton = UIAlertAction(title: dismissButtonTitle, style: .cancel) { _ in
@@ -40,7 +41,7 @@ final class ErrorHandler {
         currentVC.present(alertView, animated: true)
     }
     
-    static func showAlert(title: String, msg: String, actionButtonTitle: String = "Aceptar", action: (()->())? = nil, dismissButtonTitle: String = "Cancelar", dismissAction: (()->())? = nil) {
+    static func showAlert(title: String, msg: String, actionButtonTitle: String = Localized.accept, action: (()->())? = nil, dismissButtonTitle: String = Localized.cancel, dismissAction: (()->())? = nil) {
         
         let alertView = UIAlertController(title: title, message: msg, preferredStyle: .alert)
         
